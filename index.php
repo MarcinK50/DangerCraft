1ed61d3262c02b10baf9bb006bfcf4387ce3b0d4
Marcin Kowalicki <kowalickimarcin13@gmail.com>
2021-02-04 18:20:09 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,351 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>DangerCraft.Eu | SKLEP</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script type='text/javascript' src='//cdn.jsdelivr.net/jquery.marquee/1.3.1/jquery.marquee.min.js'></script>
+<script src="HTTPS://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script type='text/javascript' src='HTTPS://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js'></script>
+<link rel="stylesheet" href="./bootstrap/cerulean/bootstrap.css" media="screen">
+<link rel="stylesheet" href="./css/fontello.css" media="screen">
+
+<script src="./bootstrap/bootstrap.min.js"></script>
+<script src="./bootstrap/usebootstrap.js"></script>
+
+<link rel="shortcut icon" href="https://i.imgur.com/d2J5pPm.png" />
+
+<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+
+
+<style>
+BODY{
+background-color: #c4c5cc;
+background-image: url(http://bydaniello.pl/bgbd.png);
+background-repeat: no-repeat;
+background-attachment: fixed;
+background-position: center;
+background-size: cover;
+
+}
+.menu_tekst
+{
+text-decoration:none;	
+color:white;
+}
+:hover.menu_tekst
+{
+text-decoration:none;	
+color:white;
+}
+.menu_tekst:checked  
+{
+text-decoration:none;	
+color:white;	
+}
+</style>
+
+<script type="text/javascript">
+    $(function () {
+        $("[rel='tooltip']").tooltip({html: true});
+		
+
+    });
+	
+</script>
+</head>
+<body>
+    <div class="container">
+       </br>
+	   
+			<ul class="breadcrumb" style="background-color:black;">
+
+			     <li>
+				 <a class="menu_tekst" href="#"> DangerCraft.Eu | SKLEP</a>
+				    </li>
+					
+					
+					
+                    
+					
+					
+					
+                        </li>
+						</ul>
+
+            
+				
+				
+            </div>
+     
+    </div>
+
+<div class="container">
+<a href="./index.php"><img src="https://i.imgur.com/d2J5pPm.png"></a>
+	
+<nav class="navbar navbar-default">
+<div class="container-fluid">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+<span class="sr-only">Nawigacja</span>
+<span class="icon-bar"></span>
+</button>
+<a class="navbar-brand" href="index.php"></a>
+</div>
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+<ul class="nav navbar-nav">
+<li><a href="./index.php">Strona Główna</a></li> <span class="sr-only"></span></a></li><li><a href="./sklepy.php">Sklepy</a></li> <span class="sr-only"></span></a></li><li><a href="./statystyki.php">Statystyki</a></li> <span class="sr-only"></span></a></li><li><a href="./bany.php">Bany</a></li> <span class="sr-only"></span></a></li><li><a href="./zgloszenia.php">Zgłoszenia</a></li> <span class="sr-only"></span></a></li>
+<li><a href="./regulamin">Regulamin</a></li> <span class="sr-only"></span></a></li>
+</ul>
+     
+		 
+<ul class="nav navbar-nav navbar-right">
+ <li><a href="#"  data-toggle="modal" data-target=".bs-example-modal-lg" target="_blank">Zrealizuj voucher <span class="glyphicon glyphicon-ok-circle" aria-hidden="true"></span></a></li>
+</ul>
+</nav>
+	
+<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+<div class="modal-dialog" role="document">
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+<h4 class="modal-title" id="exampleModalLabel">Realizacja voucheru</h4>
+</div>
+<div class="modal-body">
+<form action="voucher.php" style="margin: 10px 0;" method="post" >
+<div class="form-group">
+<label class="control-label">Nick:</label>
+<input type="text" required type="text" name="nick" class="form-control">
+</div>
+<div class="form-group">
+<label class="control-label">Serwer:</label>
+<select name="id" class="form-control">
+<option value="1">Dangercraft.eu </option> 
+</select>
+</div>
+<div class="form-group">
+<label for="recipient-name" class="control-label">Voucher:</label>
+<input type="text" required type="text"  name="voucher" class="form-control">
+</div>
+<input type="hidden" name="token" value="d298b709b02e87283bf85930002056db">
+<input type="hidden" name="sprawdz" value="sprawdz">
+<button type="submit" class="btn btn-large btn-block btn-success">Odbierz usługę</button>
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
+ <div class="container">
+<div class="row">
+<div class="col-md-12">
+<ul class="breadcrumb">
+<li><a href="index.php">Strona główna</a> / </li>
+</ul>
+    
+
+<div id="myCarousel" class="carousel slide">
+<ol class="carousel-indicators">
+
+<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+<li data-target="#myCarousel" data-slide-to="1"></li>
+  </ol>
+  
+  <div class="carousel-inner">
+  
+      <div class="item active">
+      <img src="https://images7.alphacoders.com/556/thumb-1920-556730.jpg">     
+	  <div class="container">
+        <div class="carousel-caption">
+          <p></p>
+        </p>
+        </div>
+      </div>
+    </div>
+	
+	
+      <div class="item">
+<img src="https://images7.alphacoders.com/556/thumb-1920-556730.jpg">  
+	<div class="container">
+        <div class="carousel-caption">
+        </div>
+      </div>
+    </div>
+		
+  	
+	
+  </div>
+  
+
+  
+
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="icon-prev"></span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="icon-next"></span>
+  </a>  
+
+</div>
+</br>
+  <div class="well">
+<div class="row">
+<div class="col-md-8">
+<div class="panel panel-default shadow">	
+<div class="panel-heading">
+<b><span style="font-family: 'Roboto Condensed', sans-serif; font-size: 20px;">Newsy</span></b>	
+<a role="button" data-toggle="collapse" data-parent="#accordion"  aria-expanded="true" aria-controls="collapseOne">
+<span class="icon-arrow-right pull-right"> <i class="fa fa-angle-down fa-2x"></i></span>
+</a>
+</div>
+ </div> 
+
+    
+	
+	
+
+   
+<div class="panel panel-info shadow" >	
+<div class="panel-heading" style="background-color:red;">
+<b><span style="font-family: 'Roboto Condensed', sans-serif; font-size: 20px;">Wielka aktualizacja w SOBOTĘ (06.02) godzina 15:00.</span></b>
+</div>
+<div class="panel-body">
+<p>Wielka aktualizacja w SOBOTĘ (06.02) godzina 15:00.</p></div>
+</div>
+
+
+
+
+
+
+</div>	
+
+	
+		
+<div class="col-md-4">
+
+
+
+
+<div class="panel panel-warning">
+<div class="panel-heading">
+<h3 class="panel-title">Informacje o serwerze</h3>
+</div>
+<div class="panel-body">
+<span class="glyphicon glyphicon-globe"></span>  IP: <b>DangerCraft.eu </b></br>
+<span class="glyphicon glyphicon-wrench"></span> Wersja : <b>1.16.4</b>
+</div>
+</div>
+<div class="panel panel-info">
+<div class="panel-heading">
+<h3 class="panel-title">Ostatnio kupujący</h3>
+</div>
+<div class="panel-body">
+
+<table class="table table-striped table-hover table-bordered" style="margin:0;">
+
+<tr class="active">
+<td width="60px;" style="text-align: center;">
+Avatar
+</td>
+<td width="60px;" style="text-align: center;">
+Nick
+</td>
+
+</tr>
+
+      <tr class="active">
+<Td  style="text-align: center;">
+<a data-original-title="W1ktoria_</br>test </br>01-02-2021 12:13:56" href="#" rel="tooltip" data-placement="bottom" title="">
+<img src="https://minotar.net/avatar/W1ktoria_/50.png" style="margin-top:2px; margin-left:2px;" class="img-circle"></a> 
+</td>
+<Td  style="text-align: center;"><div style="margin-top:10px; margin-left:10px; font-size:20px;" >
+<span data-original-title="test" href="#" rel="tooltip" >
+W1ktoria_</span>
+</div></td>
+</tr>
+
+      <tr class="active">
+<Td  style="text-align: center;">
+<a data-original-title="W1ktoria_</br>test </br>01-02-2021 12:02:59" href="#" rel="tooltip" data-placement="bottom" title="">
+<img src="https://minotar.net/avatar/W1ktoria_/50.png" style="margin-top:2px; margin-left:2px;" class="img-circle"></a> 
+</td>
+<Td  style="text-align: center;"><div style="margin-top:10px; margin-left:10px; font-size:20px;" >
+<span data-original-title="test" href="#" rel="tooltip" >
+W1ktoria_</span>
+</div></td>
+</tr>
+
+
+</table>
+
+</div>
+</div>
+
+
+<div class="panel panel-info">
+<div class="panel-heading">
+<h3 class="panel-title">Najczęściej kupujący gracz</h3>
+</div>
+<div class="panel-body" style="text-align:center;">
+
+
+
+
+ <img src="https://minotar.net/avatar/W1ktoria_/100.png" data-original-title="W1ktoria_" href="#" rel="tooltip"  class="img-thumbnail" style="margin-top:2px; margin-left:2px;">
+
+ 
+
+
+
+</div>
+</div>
+
+
+<div class="panel panel-info">
+<div class="panel-heading">
+<h3 class="panel-title">Lista serwerów</h3>
+</div>
+<div class="panel-body" style="text-align:center;">
+
+<table class="table table-striped table-hover table-bordered" style="margin:0;">
+
+
+ 
+
+ <tr class="active">
+<Td  style="text-align: center;">
+
+ Dangercraft.eu</td>
+
+<td>
+<a href="./sklep.php?serwer=1" class="btn btn-primary btn-xs">Przejdź do sklepu</a>
+</td>
+
+</tr>
+ 
+
+
+</table>
+</div>
+</div>
+
+
+</div>
+            
+
+		</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+</div>
+</div>					
+<div class="container">
+<div class="well well-sm">
+<div class="row"><div class="col-md-6">
+Wykonane przez:<b> <a href="http://bojawiem55.pl/">bojawiem55.pl</a> </a>
+</div>
+</div>
